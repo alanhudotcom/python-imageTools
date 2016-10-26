2fd45415d0b3e628388213ae12549cd5af6d51c5
hexi <xi.he@baidao.com>
2016-10-26 17:15:28 +0800

    Update listBigImage.py

--- listBigImage.py
+++ listBigImage.py
@@ -6,6 +6,7 @@ import os
 from PIL import Image
 import sys
 from ListImage import ListImage
+import subprocess
 
 action_st_size = 0
 action_image_size = 1
@@ -18,12 +19,18 @@ param = {
 }
 
 def is_big_st_size(image):
-    return os.stat(image).st_size >= param['st_size']
+    return get_st_size(image) >= param['st_size']
+
+def get_st_size(image):
+    return os.stat(image).st_size
 
 def is_big_image_size(image):
+    (width, height) = get_size(image)
+    return width >= param['image_size'] and height >= param['image_size']
+
+def get_size(image):
     with Image.open(image) as im:
-        (width, height) = im.size
-        return width >= param['image_size'] and height >= param['image_size']
+        return im.size
 
 def is_and_big_size(image):
     return is_big_st_size(image) and is_big_image_size(image)
@@ -73,7 +80,24 @@ if __name__ == '__main__':
         imageFiles = list(filter(is_and_big_size, imageFiles))
 
 
-    print('===jpg or webp %d files ------------------------->' % len(imageFiles))
-    print(imageFiles)
+    print('===big images %d files ------------------------->' % len(imageFiles))
+
+    if len(imageFiles) > 0:
+        tempDir = "{baseDir}/build/bigImages".format(baseDir=param['baseDir'])
+        subprocess.getoutput("rm -rf {tempDir}".format(tempDir=tempDir))
+        subprocess.getoutput("mkdir {tempDir}".format(tempDir=tempDir))
+
+        for f in imageFiles:
+            subprocess.getoutput("cp {fileName} {tempDir}".format(tempDir=tempDir, fileName=f))
+            info = ''
+            if param['action'] == action_st_size:
+                info += "st_size: " + get_st_size(f)
+            elif param['action'] == action_image_size:
+                info += "size: " + str(get_size(f))
+            else:
+                info += "st_size: %s, size: %s" % (get_st_size(f), str(get_size(f)))
+
+            print("%s, %s" % (f, info))
+        
 
 
